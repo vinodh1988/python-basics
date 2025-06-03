c2c2060ef7d148ad3f01f480c4fb1ef469b3dbc4
vinodh1988 <k.c.vinodh@hotmail.com>
2025-06-03 12:00:08 +0530

    latest

---
+++ for.py
@@ -0,0 +1,13 @@
+# Example 1: Simple range
+for i in range(5):
+    print("Example 1:", i)
+
+print("----------------------------------------------------")
+# Example 2: Range with start and stop
+for j in range(2, 7):
+    print("Example 2:", j)
+
+print("----------------------------------------------------")
+# Example 3: Range with start, stop, and step
+for k in range(10, 0, -2):
+    print("Example 3:", k)
