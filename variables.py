117258d8cb67a92b3718b5a35af48ef74617a0a0
vinodh1988 <k.c.vinodh@hotmail.com>
2025-06-03 10:05:34 +0530

    latest

---
+++ variables.py
@@ -0,0 +1,20 @@
+a=20 # Integer
+b=30.4 # Float
+c='Hello' # String
+d=True # Boolean
+e=None # NoneTyp
+f=3+4j  # Complex number
+g=0x43bc # Hexadecimal
+h=0o1234 # Octal
+i=0b1010 # Binary
+
+print(a, b, c, d, e, f, g, h, i)
+print("A",a,"Type of a is",type(a))
+print("B",b,"Type of b is",type(b))
+print("C",c,"Type of c is",type(c))
+print("D",d,"Type of d is",type(d))
+print("E",e,"Type of e is",type(e))
+print("F",f,"Type of f is",type(f))
+print("G",g,"Type of g is",type(g))
+print("H",h,"Type of h is",type(h))
+print("I",i,"Type of i is",type(i))
