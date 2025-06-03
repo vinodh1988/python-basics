5fda7f46d1afb537d4a1fbd4850f2944d22f982c
vinodh1988 <k.c.vinodh@hotmail.com>
2025-06-03 12:50:22 +0530

    latest

--- stringproblems.py
+++ stringproblems.py
@@ -6,3 +6,11 @@ if string.lower() == string.lower()[::-1]:
     print("Palindrome")
 else:
     print("Not a palindrome")
+
+string= input("Enter a string to find the largest word: ")
+words = string.split()
+if words:
+    largest = max(words, key=len) 
+    print("Largest word:", largest)
+else:
+    print("No words found.")
